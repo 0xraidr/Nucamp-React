b52cf382ee824cefb66b32e56ef83991fcec2868
raidr <decentralizedju@gmail.com>
2022-11-26 12:53:53 -0500

    update

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,7 +1,11 @@
 export const formatDate = (date) => {
-  // return new Intl.DateTimeFormat("en-US", {
-  //   year: "numeric",
-  //   month: "short",
-  //   day: "2-digit",
-  // }).format(new Date(Date.parse(date)));
+  if (!date) {
+    return;
+  }
+
+  return new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "2-digit",
+  }).format(new Date(Date.parse(date)));
 };
